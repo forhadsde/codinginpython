a61fa6466ddd8b7abe6da192d3e667c11d3496be
Md Forhadul Islam <forhadreza1596@gmail.com>
2025-06-04 01:15:52 +0100

    update

--- CP/threeSum.py
+++ CP/threeSum.py
@@ -1,9 +1,9 @@
 from typing import List
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-         res = []
-         nums.sort()
-         for i, a in enumerate(nums):
+        res = []
+        nums.sort()
+        for i, a in enumerate(nums):
             if i > 0 and a == nums[i - 1]:
                 continue
             l, r = i + 1, len(nums) -1
@@ -18,4 +18,4 @@ class Solution:
                     l += 1
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
-         return res
+        return res
